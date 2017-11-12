00addc4594af775fa740555589126178152e1127
= <chao@chao.com>
2017-11-12 17:20:50 +0700

    hi

--- src/hlc/hlc.go
+++ src/hlc/hlc.go
@@ -1,13 +1,22 @@
+/*
+Package hlc is using to get related sentences or words of Text in both language Vietnamese-English
+This package is implemented based on website hellochao.vn
+
+	For example: the input text is "he study", so we expected to get some sentences related to that text, like "He is studying" or "He is a puple"
+*/
 package hlc
 
-type HlcRequest struct {
+// Request is a request information for translating
+type Request struct {
 	Text   string //Input text to translate
-	Cookie string
+	Cookie string //Must manually login and get the cookie from browser
 }
 
-type HlcResponse struct {
+//Response is a response of Request
+type Response struct {
 }
 
-type HlcTranslator interface {
-	Search(info HlcRequest) string
+//Translator the translator
+type Translator interface {
+	Translate(info Request) Response //Send Request and get Response from hellochao
 }
